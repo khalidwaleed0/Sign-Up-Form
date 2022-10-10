94bf13dabf38ad7035c9583d95f22b8c6895e6c9
Khalid Waleed <khalidwaleed0@outlook.com>
2022-10-10 22:15:19 +0200

    Added email check and password confirmation check

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 /** @HTMLElement */
 let passwordField = document.querySelector("input[name='password']");
+let passwordConfirmField = document.querySelector("input[name='confirm-password']");
 let conditions = document.querySelector(".password-conditions");
 let minCharCountCondition = document.querySelector(".min-char-count-check");
 let oneLetterCondition = document.querySelector(".one-letter-check");
 let oneCharCondition = document.querySelector(".one-char-check");
+let passwordMismatch = document.querySelector("p.password-mismatch");
 
 passwordField.addEventListener("input", () => {
-	console.log(passwordField.value);
-    minCharCountCondition.setAttribute("checked", passwordField.value.length >= 8);
-    oneLetterCondition.setAttribute("checked", passwordField.value.length >= 1);
-    oneCharCondition.setAttribute("checked", new RegExp(/\w/).test(passwordField.value));
+	minCharCountCondition.setAttribute("checked", passwordField.value.length >= 8);
+	oneLetterCondition.setAttribute("checked", passwordField.value.length >= 1);
+	oneCharCondition.setAttribute("checked", new RegExp(/\w/).test(passwordField.value));
+});
+
+passwordConfirmField.addEventListener("input", () => {
+	passwordMismatch.style.visibility = passwordField.value === passwordConfirmField.value ? "hidden" : "visible";
 });
